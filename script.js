c2060804a1684d2dd1c75ba942cf6133139abe9d
blidormf <150001374+blidormf@users.noreply.github.com>
2024-02-02 22:25:09 +0530

    Add functionality to draw on mouse down

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let isDragging = false;
+
 const newGridButton = document.querySelector('#new-grid');
 const container = document.querySelector('#container');
 
@@ -9,18 +11,23 @@ function createNewGrid() {
     const rows = prompt('Enter the number of rows');
     const columns = prompt('Enter the number of columns');
 
-    if (rows < 1 || rows > 40 || columns < 1 || columns > 40) {
+    if (isNaN(rows) || rows < 1 || rows > 40 || isNaN(columns) || columns < 1 || columns > 40) {
         alert("Please enter a number between 1 and 40!")
         return;
     }
 
     container.style.padding = (700 - 16 * rows) / 2 + 'px ' + (700 - 16 * columns) / 2 + 'px';
-    container.style.boxSizing = 'border-box';
 
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
             const square = document.createElement('div');
             square.setAttribute('id', 'square');
+            square.style.userSelect = 'none';
+            square.addEventListener('mousedown', (e) => {
+                isDragging = true;
+                rainbowColor(e);
+            });
+            square.addEventListener('mouseup', () => isDragging = false);
             square.addEventListener('mouseover', rainbowColor);
             container.appendChild(square);
         }
@@ -28,6 +35,9 @@ function createNewGrid() {
 }
 
 function rainbowColor(e) {
+    if (!isDragging)
+        return;
+
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
